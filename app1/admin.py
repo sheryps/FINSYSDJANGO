8a5c475a3b5748d967bd0f2d61da47f7155945dc
sheryps <sherrygeorge48@gmail.com>
2022-07-11 17:27:09 +0530

    update

---
+++ app1/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+
+admin.site.register(ProductModel)
+admin.site.register(ItemModel)
+admin.site.register(sign)
+admin.site.register(quality_inspection)
